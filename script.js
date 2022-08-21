683f84b60da19a445a87a61e764755306e74d034
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-20 22:57:24 -0600

    adding all selections to variables and making roll button work on the player 1

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const score0El = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const imgEl = document.querySelector('img');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+score1El.textContent = 0;
+score0El.textContent = 0;
+let currentScore = 0;
+
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  imgEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
+});

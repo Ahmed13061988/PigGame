8c8a7489972670fb9511138ec96252692c46e16e
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-21 02:18:00 -0600

    finishing the new button

--- script.js
+++ script.js
@@ -12,13 +12,27 @@ const current1El = document.getElementById('current--1');
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
 
-score1El.textContent = 0;
-score0El.textContent = 0;
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let totalScore = 0;
-let gameFinished = true;
+let scores, currentScore, activePlayer, gameFinished;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  gameFinished = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score1El.textContent = 0;
+  score0El.textContent = 0;
+};
+init();
 
 const swithPlayer = function () {
   currentScore = 0;
@@ -69,15 +83,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  //   diceEl.classList.add('hidden');
-  //   score0El.textContent = 0;
-  //   score1El.textContent = 0;
-  //   current0El.textContent = 0;
-  //   current1El.textContent = 0;
-
-  //   document.querySelector('.player').classList.remove('player--winner');
-  //   player0.classList.add('player--active');
-  //   gameFinished = true;
-  window.location.reload();
-});
+btnNew.addEventListener('click', init);

d8df2f832b3dd885bfe3cab34e6d3a62b58a21e8
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-20 23:32:50 -0600

    swithicng between active players

--- script.js
+++ script.js
@@ -9,10 +9,14 @@ const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 
 score1El.textContent = 0;
 score0El.textContent = 0;
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 diceEl.classList.add('hidden');
 
@@ -22,7 +26,12 @@ btnRoll.addEventListener('click', function () {
   imgEl.src = `dice-${dice}.png`;
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
 });
